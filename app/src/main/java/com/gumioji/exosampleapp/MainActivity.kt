dec41ef94d8d274cd26e7c405b731c02399acf8b
Gumio <quran.gumi3134@gmail.com>
2020-03-04 04:31:14 +0900

    adaptive streaming

--- app/src/main/java/com/gumioji/exosampleapp/MainActivity.kt
+++ app/src/main/java/com/gumioji/exosampleapp/MainActivity.kt
@@ -5,10 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -17,7 +15,10 @@ import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import android.view.View.SYSTEM_UI_FLAG_LOW_PROFILE
 import android.annotation.SuppressLint
-import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -37,36 +38,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun initPlayer() {
         val mediaSource = buildMediaSource()
-
-        mediaPlayer = SimpleExoPlayer.Builder(this).build().apply {
-            playWhenReady = playWhenReady
-            seekTo(currentWindow, playbackPosition)
-            prepare(mediaSource, false, false)
+        val trackSelector = DefaultTrackSelector(this).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())
         }
+
+        mediaPlayer = SimpleExoPlayer.Builder(this)
+            .setTrackSelector(trackSelector)
+            .build()
+            .apply {
+                playWhenReady = playWhenReady
+                seekTo(currentWindow, playbackPosition)
+                prepare(mediaSource, false, false)
+            }
 
         playerView.player = mediaPlayer
     }
 
     private fun buildMediaSource(): MediaSource {
         val dataSourceFactory = DefaultDataSourceFactory(this, "exoplayer-sample-app")
-        val uri = listOf<Uri>(
-            Uri.parse(getString(R.string.media_url_mp4)),
-            Uri.parse(getString(R.string.google_sample_url)),
-            Uri.parse(getString(R.string.media_url_mp4)),
-            Uri.parse(getString(R.string.google_sample_url)),
-            Uri.parse(getString(R.string.media_url_mp4)),
-            Uri.parse(getString(R.string.google_sample_url))
-        )
+        val uri = Uri.parse(getString(R.string.media_url_dash))
 
-        val mediaSourceList = mutableListOf<MediaSource>().apply {
-            uri.forEach {
-                val mediaSource =
-                    ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(it)
-                add(mediaSource)
-            }
-        }
-
-        return ConcatenatingMediaSource(*mediaSourceList.toTypedArray())
+        return DashMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
     }
 
     override fun onResume() {
